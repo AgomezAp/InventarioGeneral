750dfb2c3942bccc709ed03b8eb698e271dbbf14
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2026-02-16 09:23:57 -0500

    environments

--- FrontEndMaestros/src/environments/environment.ts
+++ FrontEndMaestros/src/environments/environment.ts
@@ -1,10 +1,10 @@
-/* export const environment = {
+export const environment = {
   production: true,
   apiUrl: 'https://api1.inventarioap.com/',
-}; */
+};
 
 
-export const environment = {
+/* export const environment = {
   production: false,
   apiUrl: 'http://localhost:3010/',
-};
+}; */
